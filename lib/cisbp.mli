50871576a56e29ba0c1f87301eb44cc0310dcef0
Philippe Veber <philippe.veber@gmail.com>
2019-12-17 05:20:10 +0100

    removed previously included Cisbp module

--- lib/cisbp.mli
+++
@@ -1,7 +0,0 @@
-open Bistro
-
-val fetch_pwm_archive : directory pworkflow
-
-val fetch_tf_information : tsv pworkflow
-
-val annotated_motifs : (Gzt.Cisbp.Motif.t * Gzt.Cisbp.TF_information.item list) list workflow
