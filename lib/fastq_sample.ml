42f0a1099970d402c520609aa0df91e42ecb681b
Philippe Veber <philippe.veber@gmail.com>
2020-02-04 17:37:54 +0100

    Fastq_dataset -> Fastq_sample
    type to represent a possibly gzipped single-end or paired-end list of
    fastq files.

--- lib/fastq_sample.ml
+++ lib/fastq_sample.ml
@@ -1,7 +1,23 @@
 open Core_kernel
 open Bistro
 
+type t =
+  | Fq of fastq pworkflow list SE_or_PE.t
+  | Fq_gz of fastq gz pworkflow list SE_or_PE.t
+
+let is_single_end = function
+  | Fq (Single_end _)
+  | Fq_gz (Single_end _ ) -> true
+  | Fq (Paired_end _)
+  | Fq_gz (Paired_end _) -> false
+
+let dep = function
+  | Fq se_or_pe -> SE_or_PE.map se_or_pe ~f:(List.map ~f:Shell_dsl.dep)
+  | Fq_gz se_or_pe -> SE_or_PE.map se_or_pe ~f:(List.map ~f:Bistro_unix.Cmd.gzdep)
+
+
 type source =
+  | Fastq_url of string list SE_or_PE.t
   | Fastq_gz_url of string list SE_or_PE.t
   | SRA_dataset of { srr_ids : string list ;
                      library_type : [`single_end | `paired_end] }
@@ -17,8 +33,10 @@ module Make(Data : Data) = struct
     then Bistro_unix.wget url
     else Workflow.input url
 
-  let fastq_gz x =
+  let rec fastq_gz x =
     match Data.source x with
+    | Fastq_url _ ->
+      SE_or_PE.map (fastq x) ~f:(List.map ~f:Bistro_unix.gzip)
     | Fastq_gz_url uris ->
       SE_or_PE.map uris ~f:(List.map ~f:unsafe_file_of_url)
     | SRA_dataset { srr_ids ; library_type } ->
@@ -33,7 +51,19 @@ module Make(Data : Data) = struct
       | `single_end ->
         Single_end (List.map srr_ids ~f:(fun id -> Sra_toolkit.fastq_dump_gz (`id id)))
 
-  let fastq x = SE_or_PE.map ~f:(List.map ~f:Bistro_unix.gunzip) (fastq_gz x)
+  and fastq x =
+    match Data.source x with
+    | Fastq_url uris ->
+      SE_or_PE.map uris ~f:(List.map ~f:unsafe_file_of_url)
+    | Fastq_gz_url _
+    | SRA_dataset _ ->
+      SE_or_PE.map ~f:(List.map ~f:Bistro_unix.gunzip) (fastq_gz x)
+
+  let fastq_sample x =
+    match Data.source x with
+    | Fastq_url _ -> Fq (fastq x)
+    | SRA_dataset _
+    | Fastq_gz_url _ -> Fq_gz (fastq_gz x)
 
   let fastqc x =
     SE_or_PE.map (fastq_gz x) ~f:(List.map ~f:FastQC.fastqc_gz)
--- lib/fastq_sample.mli
+++ lib/fastq_sample.mli
@@ -1,6 +1,15 @@
 open Bistro
 
+type t =
+  | Fq of fastq pworkflow list SE_or_PE.t
+  | Fq_gz of fastq gz pworkflow list SE_or_PE.t
+
+val is_single_end : t -> bool
+
+val dep : t -> Shell_dsl.template list SE_or_PE.t
+
 type source =
+  | Fastq_url of string list SE_or_PE.t
   | Fastq_gz_url of string list SE_or_PE.t
   | SRA_dataset of { srr_ids : string list ;
                      library_type : [`single_end | `paired_end] }
@@ -13,5 +22,6 @@ end
 module Make(Data : Data) : sig
   val fastq : Data.t -> fastq pworkflow list SE_or_PE.t
   val fastq_gz : Data.t -> fastq gz pworkflow list SE_or_PE.t
+  val fastq_sample : Data.t -> t
   val fastqc : Data.t -> FastQC.report pworkflow list SE_or_PE.t
 end
