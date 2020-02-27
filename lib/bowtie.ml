pveber/biotope

commit c68f127cf3353e579a00105efa01ad9c9d0e11f1

Use Fastq_sample in Bowtie and Hisat

lib/bowtie.ml
@@ -16,16 +16,32 @@ let bowtie_build ?packed ?color fa =
     ]
   ]
 
-let bowtie ?l ?e ?m ?fastq_format ?n ?v ?maxins index fastq_files =
-  let args = match fastq_files with
-    | SE_or_PE.Single_end fqs -> list dep ~sep:"," fqs
-    | Paired_end (fqs1, fqs2) ->
-      seq [
-        opt "-1" (list dep ~sep:",") fqs1 ;
-        string " " ;
-        opt "-2" (list dep ~sep:",") fqs2
-      ]
+let bowtie_style_fastq_args fq_samples =
+  let (fqs, fqs1, fqs2), (fqs_gz, fqs1_gz, fqs2_gz) =
+    Fastq_sample.explode fq_samples
   in
+  let fqs = List.map fqs ~f:dep @ List.map fqs_gz ~f:Bistro_unix.Cmd.gzdep in
+  let fqs1 = List.map fqs1 ~f:dep @ List.map fqs1_gz ~f:Bistro_unix.Cmd.gzdep in
+  let fqs2 = List.map fqs2 ~f:dep @ List.map fqs2_gz ~f:Bistro_unix.Cmd.gzdep in
+  List.filter_opt [
+    if List.is_empty fqs then None else Some (opt "-U" (seq ~sep:",") fqs) ;
+    if List.is_empty fqs1 then None else Some (
+        seq [
+          opt "-1" (seq ~sep:",") fqs1 ;
+          string " " ;
+          opt "-2" (seq ~sep:",") fqs2
+        ])
+  ]
+  |> seq ~sep:" "
+
+let qual_option = function
+  | Fastq.Solexa  -> "--solexa-quals"
+  | Fastq.Sanger -> "--phred33-quals"
+  | Fastq. Phred64 -> "--phred64-quals"
+
+let bowtie ?l ?e ?m ?fastq_format ?n ?v ?maxins ?(additional_samples = []) index fq_sample =
+  let fq_samples = fq_sample :: additional_samples in
+  let fq_args = bowtie_style_fastq_args fq_samples in
   Workflow.shell ~descr:"bowtie" ~mem:(Workflow.int (3 * 1024)) ~np:8 [
     cmd "bowtie" ~img [
       string "-S" ;
@@ -34,11 +50,11 @@ let bowtie ?l ?e ?m ?fastq_format ?n ?v ?maxins index fastq_files =
       option (opt "-e" int) e ;
       option (opt "-m" int) m ;
       option (opt "-v" int) v ;
-      option (opt "-q" (Bowtie2.qual_option % string)) fastq_format ;
+      option (opt "-q" (qual_option % string)) fastq_format ;
       opt "-p" Fn.id np ;
       option (opt "--maxins" int) maxins ;
       seq [dep index ; string "/index"] ;
-      args ;
+      fq_args ;
       dest ;
     ]
   ]

lib/bowtie.mli
@@ -10,6 +10,11 @@ val bowtie :
   ?fastq_format:Fastq.format ->
   ?n:int -> ?v:int ->
   ?maxins:int ->
+  ?additional_samples:Fastq_sample.t list ->
   [`bowtie_index] directory ->
-  #fastq file list SE_or_PE.t ->
+  Fastq_sample.t ->
   sam file
+
+
+val qual_option : Fastq.format -> string
+val bowtie_style_fastq_args : Fastq_sample.t list -> Shell_dsl.template

lib/bowtie2.ml
@@ -28,11 +28,6 @@ let bowtie2_build ?large_index ?noauto ?packed ?bmax ?bmaxdivn ?dcv ?nodc ?noref
     ]
   ]
 
-let qual_option = function
-  | Fastq.Solexa  -> "--solexa-quals"
-  | Fastq.Sanger -> "--phred33-quals"
-  | Fastq. Phred64 -> "--phred64-quals"
-
 let flag_of_preset mode preset =
   let flag = match preset with
     | `very_fast -> "--very-fast"
@@ -62,24 +57,7 @@ let bowtie2
     ?no_unal ?seed
     ?fastq_format ?(additional_samples = []) index fq_sample =
   let fq_samples = fq_sample :: additional_samples in
-  let args =
-    let (fqs, fqs1, fqs2), (fqs_gz, fqs1_gz, fqs2_gz) =
-      Fastq_sample.explode fq_samples
-    in
-    let fqs = List.map fqs ~f:dep @ List.map fqs_gz ~f:Bistro_unix.Cmd.gzdep in
-    let fqs1 = List.map fqs1 ~f:dep @ List.map fqs1_gz ~f:Bistro_unix.Cmd.gzdep in
-    let fqs2 = List.map fqs2 ~f:dep @ List.map fqs2_gz ~f:Bistro_unix.Cmd.gzdep in
-    List.filter_opt [
-      if List.is_empty fqs then None else Some (opt "-U" (seq ~sep:",") fqs) ;
-      if List.is_empty fqs1 then None else Some (
-          seq [
-              opt "-1" (seq ~sep:",") fqs1 ;
-              string " " ;
-              opt "-2" (seq ~sep:",") fqs2
-            ])
-    ]
-    |> seq ~sep:" "
-  in
+  let args = Bowtie.bowtie_style_fastq_args fq_samples in
   Workflow.shell ~descr:"bowtie2" ~mem:(Workflow.int (3 * 1024)) ~np:8 [
     cmd "bowtie2" ~img [
       option (opt "--skip" int) skip ;
@@ -106,7 +84,7 @@ let bowtie2
       option (flag string "--no-unal") no_unal ;
       opt "--threads" Fn.id np ;
       option (opt "--seed" int) seed ;
-      option (opt "-q" (qual_option % string)) fastq_format ;
+      option (opt "-q" (Bowtie.qual_option % string)) fastq_format ;
       opt "-x" (fun index -> seq [dep index ; string "/index"]) index ;
       args ;
       opt "-S" Fn.id dest ;

lib/hisat2.ml
@@ -37,19 +37,12 @@ let hisat2
     ?k
     ?minins ?maxins ?orientation ?no_mixed ?no_discordant
     ?seed
+    ?(additional_samples = [])
     index
-    fqs
+    fq
   =
-  let args = match fqs with
-    | SE_or_PE.Single_end fqs ->
-      opt "-U" (list dep ~sep:",") fqs
-    | Paired_end (fqs1, fqs2) ->
-      seq [
-        opt "-1" (list dep ~sep:",") fqs1 ;
-        string " " ;
-        opt "-2" (list dep ~sep:",") fqs2
-      ]
-  in
+  let fq_samples = fq :: additional_samples in
+  let args = Bowtie.bowtie_style_fastq_args fq_samples in
   Workflow.shell ~descr:"hisat2" ~mem:(Workflow.int (4 * 1024)) ~np:8 [
     cmd "hisat2" ~img [
       option (opt "--skip" int) skip ;
@@ -64,8 +57,7 @@ let hisat2
       option (flag string "--no-discordant") no_discordant  ;
       opt "--threads" Fn.id np ;
       option (opt "--seed" int) seed ;
-      option (opt "-q" (Bowtie2.qual_option % string)) fastq_format ;
-
+      option (opt "-q" (Bowtie.qual_option % string)) fastq_format ;
       opt "-x" (fun index -> seq [dep index ; string "/index"]) index ;
       args ;
       opt "-S" Fn.id dest ;

lib/bowtie2.mli
@@ -46,5 +46,3 @@ val bowtie2 :
   [`bowtie2_index] directory ->
   Fastq_sample.t ->
   sam file
-
-val qual_option : Fastq.format -> string

lib/examples/zhou2011.ml
@@ -3,6 +3,7 @@
    Datasets: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE29506
 *)
 open Core
+open Biotk
 open Biotope
 open Bistro_utils
 
@@ -25,17 +26,19 @@ let genome = Ucsc_gb.genome_sequence `sacCer2
 let genome_2bit = Ucsc_gb.genome_2bit_sequence `sacCer2
 
 let srr_id = function
-  | `ChIP_Pho4_noPi -> [ "SRR217304" ; "SRR217305" ]
-  | `Input_WT_NoPi -> [ "SRR217324" ]
+  | `ChIP_Pho4_noPi -> List1.cons "SRR217304" [ "SRR217305" ]
+  | `Input_WT_NoPi -> List1.singleton "SRR217324"
 
-let fastq x = List.map (srr_id x) ~f:(fun id ->
+let fastq x = List1.map (srr_id x) ~f:(fun id ->
     Sra_toolkit.fastq_dump (`id id)
+    |> Fastq_sample.plain_se
   )
 
 let bowtie_index = Bowtie.bowtie_build genome
 
 let mapped_reads x =
-  Bowtie.bowtie ~v:1 bowtie_index (SE_or_PE.Single_end (fastq x))
+  let Cons (fq, additional_samples) = fastq x in
+  Bowtie.bowtie ~v:1 bowtie_index ~additional_samples fq
 
 let mapped_reads_bam x =
   Samtools.indexed_bam_of_sam (mapped_reads x)

lib/hisat2.mli
@@ -33,6 +33,7 @@ val hisat2 :
   ?no_mixed:bool ->
   ?no_discordant:bool ->
   ?seed:int ->
+  ?additional_samples:Fastq_sample.t list ->
   [`hisat2_index] directory ->
-  #fastq file list SE_or_PE.t ->
+  Fastq_sample.t ->
   sam file

commit 56b7835307d1abdec28b73b0b28270541be72b0f

Bowtie: fixed bug introduced in c68f127
bowtie1 doesn't use -U option, while bowtie2 and hisat2 do

lib/bowtie.ml
@@ -16,15 +16,19 @@ let bowtie_build ?packed ?color fa =
     ]
   ]
 
-let bowtie_style_fastq_args fq_samples =
+let fastq_args version fq_samples =
   let (fqs, fqs1, fqs2), (fqs_gz, fqs1_gz, fqs2_gz) =
     Fastq_sample.explode fq_samples
   in
   let fqs = List.map fqs ~f:dep @ List.map fqs_gz ~f:Bistro_unix.Cmd.gzdep in
   let fqs1 = List.map fqs1 ~f:dep @ List.map fqs1_gz ~f:Bistro_unix.Cmd.gzdep in
   let fqs2 = List.map fqs2 ~f:dep @ List.map fqs2_gz ~f:Bistro_unix.Cmd.gzdep in
+  let single_end_option = match version with
+    | `V1 -> ""
+    | `V2 -> "-U"
+  in
   List.filter_opt [
-    if List.is_empty fqs then None else Some (opt "-U" (seq ~sep:",") fqs) ;
+    if List.is_empty fqs then None else Some (opt single_end_option (seq ~sep:",") fqs) ;
     if List.is_empty fqs1 then None else Some (
         seq [
           opt "-1" (seq ~sep:",") fqs1 ;
@@ -41,7 +45,7 @@ let qual_option = function
 
 let bowtie ?l ?e ?m ?fastq_format ?n ?v ?maxins ?(additional_samples = []) index fq_sample =
   let fq_samples = fq_sample :: additional_samples in
-  let fq_args = bowtie_style_fastq_args fq_samples in
+  let fq_args = fastq_args `V1 fq_samples in
   Workflow.shell ~descr:"bowtie" ~mem:(Workflow.int (3 * 1024)) ~np:8 [
     cmd "bowtie" ~img [
       string "-S" ;

lib/bowtie.mli
@@ -17,4 +17,7 @@ val bowtie :
 
 
 val qual_option : Fastq.format -> string
-val bowtie_style_fastq_args : Fastq_sample.t list -> Shell_dsl.template
+val fastq_args :
+  [`V1 | `V2] ->
+  Fastq_sample.t list ->
+  Shell_dsl.template

lib/bowtie2.ml
@@ -57,7 +57,7 @@ let bowtie2
     ?no_unal ?seed
     ?fastq_format ?(additional_samples = []) index fq_sample =
   let fq_samples = fq_sample :: additional_samples in
-  let args = Bowtie.bowtie_style_fastq_args fq_samples in
+  let args = Bowtie.fastq_args `V2 fq_samples in
   Workflow.shell ~descr:"bowtie2" ~mem:(Workflow.int (3 * 1024)) ~np:8 [
     cmd "bowtie2" ~img [
       option (opt "--skip" int) skip ;

lib/hisat2.ml
@@ -42,7 +42,7 @@ let hisat2
     fq
   =
   let fq_samples = fq :: additional_samples in
-  let args = Bowtie.bowtie_style_fastq_args fq_samples in
+  let args = Bowtie.fastq_args `V2 fq_samples in
   Workflow.shell ~descr:"hisat2" ~mem:(Workflow.int (4 * 1024)) ~np:8 [
     cmd "hisat2" ~img [
       option (opt "--skip" int) skip ;