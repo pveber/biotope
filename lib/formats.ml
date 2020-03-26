fb83e001a7fe29e909745a75be209d1b3da48468
Philippe Veber <philippe.veber@gmail.com>
2020-03-26 17:35:03 +0100

    new Formats module for additional file formats

---
+++ lib/formats.ml
@@ -0,0 +1,16 @@
+open Bistro
+
+class type nhx = object
+  inherit text
+  method format : [`nhx]
+end
+
+class type newick = object
+  inherit text
+  method format : [`newick]
+end
+
+class type phylip = object
+  inherit text
+  method format : [`phylip]
+end
